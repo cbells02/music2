a12568cd4c65190c2bf33f002d2593513ec00059
cbells02 <cbells002@gmail.com>
2022-11-17 01:02:55 -0600

    This is a old music player project I was working on. (not finished)

---
+++ Polymorphism.py
@@ -0,0 +1,27 @@
+class Dog():
+
+    def __init__(self,name):
+        self.name = name
+
+
+    def speak(self):
+        return self.name +" says woof"
+
+
+class Cat():
+
+    def __init__(self,name):
+        self.name = name
+
+
+    def speak(self):
+        return self.name +" says meow"
+
+
+niko = Dog("niko")
+felix = Cat("Felix")
+
+print(niko.speak())
+print(felix.speak())
+
+
